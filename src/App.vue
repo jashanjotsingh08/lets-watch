c11d68b037d70b3bf54d136c0759e4253e97970a
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2022-08-28 15:22:26 -0400

    Added styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,50 @@
 <template>
-  <router-view />
+  <div>
+    <header>
+      <router-link to="/">
+        <h1><span>Lets</span>Watch</h1>
+      </router-link>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+
+  &::selection {
+    background: transparentize($color: #42b883, $amount: 0.5);
+  }
+}
+
+body {
+  background: #35495e;
+}
+
+a {
+  text-decoration: none;
+}
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    color: #fff;
+    font-size: 28px;
+
+    span {
+      color: #42b883;
+    }
+  }
+}
+</style>
