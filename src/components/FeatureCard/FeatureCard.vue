c5e4343e21ea816c35133c005cd424219b1f9295
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2022-08-29 23:02:55 -0400

    Created feature component component

---
+++ src/components/FeatureCard/FeatureCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="feature-card">
+    <router-link to="">
+      <img
+        src="https://d16kd6gzalkogb.cloudfront.net/magazine_images/RosemarysBaby_quad_UK-1.jpg"
+        alt="A movie background"
+        class="feature-image"
+      />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.feature-card {
+  position: relative;
+
+  .feature-img {
+    display: block;
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+
+    position: relative;
+    z-index: 0;
+
+    .details {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
