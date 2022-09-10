37128287274d16d0dd61be0e96f3fbf7937b470d
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2022-09-10 00:38:50 -0400

    Added movie detail, search bar and movieCard component

---
+++ src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="search-bar">
+    <form @submit.prevent="searchMovies()" class="search-box">
+      <input
+        type="text"
+        placeholder="What are you looking for?"
+        v-model="search"
+      />
+      <input type="submit" value="Search" />
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import axios from "axios";
+import env from "@/env.js";
+export default {
+  emits: ["movieList"],
+  setup(props, context) {
+    const search = ref("");
+    const movies = ref([]);
+
+    const searchMovies = () => {
+      if (search.value != "") {
+        axios
+          .get(`http://www.omdbapi.com/?apikey=${env.apiKey}&s=${search.value}`)
+          .then((response) => {
+            if (response) {
+              movies.value = response.data.Search;
+              // eslint-disable-next-line no-undef
+              context.emit("movieList", movies.value);
+              search.value = "";
+            }
+          });
+      }
+    };
+
+    return {
+      search,
+      movies,
+      searchMovies,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.search-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+
+  input {
+    display: block;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+
+    &[type="text"] {
+      width: 100%;
+      color: #fff;
+      background-color: #496583;
+      font-size: 20px;
+      padding: 10px 16px;
+      border-radius: 8px;
+      margin-bottom: 15px;
+      transition: 0.4s;
+
+      &::placeholder {
+        color: #f3f3f3;
+      }
+
+      &::focus {
+        box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+      }
+    }
+
+    &[type="submit"] {
+      width: 100%;
+      max-width: 300px;
+      background-color: #42b843;
+      padding: 16px;
+      border-radius: 8px;
+      color: #fff;
+      font-size: 20px;
+      text-transform: uppercase;
+      transition: 0.4s;
+
+      &:active {
+        background-color: #3b8070;
+      }
+    }
+  }
+}
+</style>
