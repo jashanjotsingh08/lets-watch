1e5dd8b2de185aba91022a4a50eea25009a9432e
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2022-09-11 21:45:58 -0400

    Added Navbar Component

--- src/components/SearchBar/SearchBar.vue
+++ src/components/SearchBar/SearchBar.vue
@@ -85,7 +85,7 @@ export default {
       width: 100%;
       max-width: 300px;
       background-color: #42b843;
-      padding: 16px;
+      padding: 10px 16px;
       border-radius: 8px;
       color: #fff;
       font-size: 20px;
