348605b14c7be4cf4d939704aca6cf5aecf91c2c
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2022-09-12 23:05:28 -0400

    Removed unused code and formatted the logic

--- src/services/OmdbApi.js
+++ src/services/OmdbApi.js
@@ -3,24 +3,23 @@ import axios from "axios";
 const baseDomain = process.env.VUE_APP_OMDB_API_BASE_DOMAIN;
 const baseUrl = `${baseDomain}?apiKey=${process.env.VUE_APP_API_TOKEN}&`;
 
-// const apiResource = axios.create({ baseURL: baseUrl });
-
 const getSearchResult = async ({ searchKey, page, type, year }) => {
+  let params = {
+    s: searchKey,
+    type: type,
+    y: year,
+    page: page,
+  };
+
   if (searchKey) {
-    let params = {
-      s: searchKey,
-      type: type,
-      y: year,
-      page: page,
-    };
     return await axios.get(baseUrl, { params });
   }
 };
 
 const getItem = async ({ id, title, plot }) => {
+  let params = { i: id, t: title, plot: plot };
+
   if (id || title) {
-    console.log(id);
-    let params = { i: id, t: title, plot: plot };
     return await axios.get(baseUrl, { params });
   }
 };
